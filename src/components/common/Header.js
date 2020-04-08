14ab0252d23920f5ddcdf38f538708a15bebbc0d
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-04-08 22:10:29 +0100

    added dark mode fix

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Header = () => {
   const toogleRef = useRef(null);
@@ -9,7 +9,18 @@ const Header = () => {
     let newColor = color === "light" ? "dark" : "light";
     setColor(newColor);
     document.documentElement.setAttribute("data-theme", newColor);
+    localStorage.setItem("theme", newColor);
   };
+  useEffect(() => {
+    const currentTheme = localStorage.getItem("theme")
+      ? localStorage.getItem("theme")
+      : null;
+
+    if (currentTheme) {
+      document.documentElement.setAttribute("data-theme", currentTheme);
+    }
+    return () => {};
+  });
   return (
     <div className="header">
       <div className="inner-header">
